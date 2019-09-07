d978d7de8bc9200bb8f31e99ebc0a884e0000508
zhouke <936712486@qq.com>
2019-09-07 23:37:35 +0800

    支付页面新增支付宝支付测试接口

--- src/pages/PayOnline/PayOnLine.vue
+++ src/pages/PayOnline/PayOnLine.vue
@@ -103,7 +103,7 @@
               if(data.status!==0){
                 MessageBox.confirm(data.msg)
               }else{
-                window.location.href=data.url;
+                window.location.href=data.url; 
               }
             })
           }
